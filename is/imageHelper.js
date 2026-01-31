c1636967ae0e3c33f8a0f0580e887581d1dafe6a
rhetesi <66908912+rhetesi@users.noreply.github.com>
2026-01-31 23:06:21 +0100

    Add fileToBase64 function to convert files

---
+++ is/imageHelper.js
@@ -0,0 +1,7 @@
+export function fileToBase64(file) {
+    return new Promise(resolve => {
+        const reader = new FileReader();
+        reader.onload = e => resolve(e.target.result);
+        reader.readAsDataURL(file);
+    });
+}
